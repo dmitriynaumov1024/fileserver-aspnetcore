343f826b24b376a8d67caea16473a578f6737cdc
dmitriynaumov1024 <naumov1024@gmail.com>
2022-01-19 23:15:18 +0200

    Upload all sources at once

---
+++ FileServerApp/Handlers.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace FileServerApp
+{
+    public static class Handlers
+    {
+        public static async Task DefaultGet (HttpContext context) {
+            await context.Response.SendFileAsync("index.html");
+        }
+
+        public static async Task FileServer (HttpContext context) {
+            string relPath = (string)context.Request.Query["path"] ?? "/";
+            string path = "./wwwroot/" + relPath;
+            bool goodPath = !(path.Contains("../") || path.Contains(":")); 
+            if (goodPath) {
+                if (File.Exists(path)) {
+                    await context.Response.SendFileAsync(path);
+                }
+                else {
+                    var fsEntries = GetFsEntries(relPath);
+                    await context.Response.WriteAsJsonAsync(fsEntries);
+                }
+            }
+            else {
+                await context.Response.SendFileAsync("index.html");
+            }
+        }
+
+        static Dictionary<string, object> GetFsEntries (string relPath) {
+            string path = "./wwwroot" + relPath;
+            IEnumerable<string> subdirs = null, files = null;
+            var basedir = new DirectoryInfo(path);
+            var rootdir = new DirectoryInfo("./wwwroot");
+            string parent = "";
+
+            if (basedir.Exists) {
+                // Get subdirs and files
+                subdirs = basedir.GetDirectories().Select(item => item.Name);
+                files = basedir.GetFiles().Select(item => item.Name);
+                parent = GetRelativePath(basedir.Parent, rootdir);
+            }
+            
+            // Make a string:string dictionary
+            var resultDict = new Dictionary<string, object> {
+                { "base", relPath.EndsWith('/') ? relPath : relPath + '/' },
+                { "parent", parent },
+                { "dirs", subdirs ?? new string[0] },
+                { "files", files ?? new string[0] }
+            }; 
+
+            return resultDict;
+        }
+
+        static string GetRelativePath (DirectoryInfo directory, DirectoryInfo root)
+        {
+            string rootPath = root.FullName.Replace('\\', '/');
+            string dirPath = directory.FullName.Replace('\\', '/');
+            if (rootPath.Length > dirPath.Length) {
+                return "";
+            }
+            else {
+                return dirPath.Replace(rootPath, "");
+            }
+        }
+
+        static JsonSerializerOptions jsonOpts = new JsonSerializerOptions {
+            WriteIndented = true,
+        };
+
+        static async Task WriteAsJsonAsync<T> (this HttpResponse context, T obj, JsonSerializerOptions options = null) 
+        {
+            context.ContentType = "application/json; charset=utf-8";
+            await JsonSerializer.SerializeAsync(context.Body, obj, options);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# fileserver-aspnetcore
+# File server
+
+## Summary
+
+This is a simple file server app built with [ASP.NET Core](https://dotnet.microsoft.com/en-us/apps/aspnet) framework. You can easily browse directories and view files with this app. To get started,
+1. Download [.NET sdk](https://dotnet.microsoft.com/en-us/download/dotnet/5.0) (this project requires .NET sdk 5.0)
+2. Build the project: go to FileServerApp and `dotnet build`
+3. Go to build output (something like bin/Debug/netcoreapp3.1/)
+4. Put your files/folders to wwwroot directory in build output
+5. Launch the executable file (FileServerApp)
+6. Type `http://localhost:5000/` in your browser
+7. Enjoy!
+
+## What's inside
+
+This application's backend is written in C#. Frontend is written using [Vue.js](https://vuejs.org) framework.
+
+File Program.cs is used to initialize the web host builder and build the application.
+File Startup.cs is used to configure the application on startup.
+File Handlers.cs contains 2 request delegates:
+- DefaultGet - default fallback delegate
+- FileServer - get a file or directory info depending on request path.
+File index.html is the only frontend file. It contains all necessary markup, styles and script. 
