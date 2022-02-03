e445c5bf83aac2c91b8d888b75ca1f3f51fe4761
dmitriynaumov1024 <naumov1024@gmail.com>
2022-02-03 19:52:13 +0200

    Adopting hybrid mode
    Now directory info can be accessed not only via json, but also with a simple htp request. Known issue: assets are not cached on client side.

--- FileServerApp/Handlers.cs
+++ FileServerApp/Handlers.cs
@@ -16,44 +16,13 @@ namespace FileServerApp
         // Default request handler - sends index.html to client
         public static async Task DefaultGet (HttpContext context) 
         {
-            await context.Response.SendFileAsync("index.html");
-        }
-
-        // File server
-        public static async Task FileServer (HttpContext context) 
-        {
-            string rootPath = "./wwwroot/";
-            // Get path from current http request
-            string relPath = (string)context.Request.Query["path"] ?? String.Empty;
-            // Get full path (with root path substitution)
-            string path = rootPath + relPath;
-            // Escaping the wwwroot directory can reveal sensitive information.
-            bool goodPath = !(path.Contains("../") || path.Contains(":"));
-            if (goodPath) {
-                // If path represents a file - send it as attachment
-                if (File.Exists(path)) {
-                    await context.Response.SendFileAttachmentAsync(path);
-                }
-                // If path represents a directory - send information about its contents
-                else if (Directory.Exists(path)) {
-                    var fsEntries = FsExtensions.GetFsEntries(relPath, rootPath);
-                    await context.Response.WriteAsJsonAsync(fsEntries);
-                }
-            }
-            // Fallback
-            else {
-                await DefaultGet(context);
-            }
+            await context.Response.WriteAsync(PreprocessedIndexPage);
         }
 
         // Assets delivery
         public static async Task AssetsDelivery (HttpContext context) 
         {
-            string rootPath = "./assets";
-            // Get path from current http request
-            string relPath = (string)context.Request.Query["path"] ?? "/";
-            // Get full path (with root path substitution)
-            string path = rootPath + relPath;
+            string path = GetPathFrom(context, prefix: "./assets");
             // Escaping the root directory can reveal sensitive information.
             bool goodPath = !(path.Contains("../") || path.Contains(":"));
             // If path is safe and file exists, send the file
@@ -65,5 +34,54 @@ namespace FileServerApp
                 await context.Response.NotFound();
             }
         }
+
+        // File server
+        public static async Task FileServer (HttpContext context) 
+        {
+            string rootPath = "./wwwroot";
+            string relPath = GetPathFrom(context);
+            string path = rootPath + relPath;
+            // Escaping the wwwroot directory can reveal sensitive information.
+            bool goodPath = !(path.Contains("../") || path.Contains(":"));
+            if (goodPath) {
+                // If path represents a file - send it as attachment
+                if (File.Exists(path)) {
+                    await context.Response.SendFileAttachmentAsync(path);
+                }
+                // If path represents a directory - send information about its contents
+                else if (Directory.Exists(path)) {
+                    if (context.Request.Headers.ContainsKey("JSON-Only")) {
+                        var fsEntries = FsExtensions.GetFsEntries(relPath, rootPath);
+                        await context.Response.WriteAsJsonAsync(fsEntries);
+                    }
+                    else {
+                        var replacements = new Dictionary<string, object> {
+                            { "dirUrlPrefix", relPath }
+                        };
+                        await context.Response.SendPreprocessedHtmlAsync("index.html", replacements);
+                    }
+                }
+                else {
+                    await DefaultGet(context);
+                }
+            }
+            // Fallback
+            else {
+                await DefaultGet(context);
+            }
+        }
+
+        static string GetPathFrom (HttpContext context, string prefix = "") 
+        {
+            // Get path from current http request
+            string relPath = (string)context.Request.Query["path"] ?? String.Empty;
+            // Get full path (with root path substitution)
+            return prefix + relPath;
+        }
+
+        static string PreprocessedIndexPage = HtmlPreprocessHelper.GetPreprocessedHtml (
+            "index.html", 
+            new Dictionary<string, object>{{"dirUrlPrefix", ""}}
+        );
     }
 }
--- FileServerApp/HttpResponseExtensions.cs
+++ FileServerApp/HttpResponseExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -9,10 +10,10 @@ namespace FileServerApp
 {
     public static class HttpResponseExtensions
     {
-        public static async Task WriteAsJsonAsync<T> (this HttpResponse context, T obj, JsonSerializerOptions options = null) 
+        public static async Task WriteAsJsonAsync<T> (this HttpResponse response, T obj, JsonSerializerOptions options = null) 
         {
-            context.ContentType = "application/json; charset=utf-8";
-            await JsonSerializer.SerializeAsync(context.Body, obj, options);
+            response.ContentType = "application/json; charset=utf-8";
+            await JsonSerializer.SerializeAsync(response.Body, obj, options);
         }
 
         public static async Task SendFileAttachmentAsync (this HttpResponse response, string filepath) 
@@ -31,6 +32,15 @@ namespace FileServerApp
             Mime.TryGetContentType(filepath, out contentType);
             response.Headers.Append ("Content-Type", contentType);
             await response.SendFileAsync(filepath);
+        }
+
+        public static async Task SendPreprocessedHtmlAsync (
+                this HttpResponse response, 
+                string filepath, 
+                IDictionary<string, object> replacements) 
+        {
+            string preprocessingResult = HtmlPreprocessHelper.GetPreprocessedHtml(filepath, replacements, useCache: true);
+            await response.WriteAsync(preprocessingResult);
         }
 
         public static async Task NotFound (this HttpResponse response)
